e04c847c782663daaa1207956ab244679a18a4e3
Raul Muñoz <rauliko1999@hotmail.com>
2021-03-11 23:13:04 +0100

    Hasta ads android bien

--- src/app/services/admob.service.ts
+++ src/app/services/admob.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
+
 import { PluginListenerHandle, Plugins } from '@capacitor/core';
 import { AdOptions, AdSize, AdPosition } from '@capacitor-community/admob';
 import { ConfiguracionService } from './configuracion.service';
 import { Platform } from '@ionic/angular';
 const { AdMob } = Plugins;
-=======
-import { Platform } from '@ionic/angular';
-import { ConfiguracionService } from './configuracion.service';
->>>>>>> 590fda5f6f8fb3a6e31bb7fbd56e6635929b39ec
 
 @Injectable({
   providedIn: 'root'
@@ -20,8 +16,7 @@ export class AdmobService {
   idUsuarioActual = JSON.parse(localStorage.getItem('user')).uid;
   cargando = false;
   //REWARD VIDEO CONFIG.
-<<<<<<< HEAD
-  options: AdOptions = {
+ options: AdOptions = {
     isTesting: true, // KEEP DURING CODING, REMOVE AT PROD.
     adId: 'ca-app-pub-6581604974415927/5046711261'
   };
@@ -58,60 +53,4 @@ export class AdmobService {
         this.cantidadAnterior = cantidadAnterior;
         AdMob.prepareRewardVideoAd(this.options);
       }
-      }
-=======
-  RewardVideoConfig: AdMobFreeRewardVideoConfig = {
-  isTesting: true, // KEEP DURING CODING, REMOVE AT PROD.
-  autoShow: true//,
-  //id: "ID GENERATED AT ADMOB ca-app-pub FOR PROD"
-  };
-    constructor(
-      public platform: Platform,
-      private admobFree: AdMobFree,
-      private configuracionService: ConfiguracionService
-      ) {
-      //LOAD ADS AT PLATFORM READY PROMISE.
-      platform.ready().then(()=>{
-      //REWARD VIDEO
-      this.admobFree.rewardVideo.config(this.RewardVideoConfig);
-      this.admobFree.rewardVideo.prepare().then(() => {
-      console.log('REWARD VIDEO LOADED')
-      }).catch(e =>
-      console.log('PROBLEM LOADING REWARDVIDEO: ', e)
-      );
-      });
-  
-      this.admobFree.on('admob.rewardvideo.events.REWARD').subscribe(()=> {
-        console.log("PREMIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-        if (this.tipoReward === 'P'){
-          this.configuracionService.SumarPersona(this.idUsuarioActual,this.cantidadAnterior)
-        } else if (this.tipoReward === 'R'){
-          this.configuracionService.SumarRegalo(this.idUsuarioActual,this.cantidadAnterior)
-        }
-        this.cargando= false;
-      })
-      }
-      
-      ShowRewardVideo(tipo: string, cantidadAnterior: number) {
-        this.cargando = true;
-      //CHECK AND SHOW REWARDVIDEO
-      this.admobFree.rewardVideo.isReady().then(() => {
-      //AT .ISREADY SHOW
-      this.admobFree.rewardVideo.prepare().then(() => {
-        this.cargando= false;
-        console.log('BANNER LOADED')
-        this.tipoReward = tipo;
-        this.cantidadAnterior = cantidadAnterior;
-      })
-      .catch(e => {
-        this.cargando= false;
-        console.log('PROBLEM LOADING REWARD VIDEO: ', e)
-    }  );
-      })
-      .catch(e => {
-        this.cargando= false;
-        console.log('PROBLEM LOADING REWARD VIDEO: ', e)
-      }  );
-      }
-      }
->>>>>>> 590fda5f6f8fb3a6e31bb7fbd56e6635929b39ec
+    }
